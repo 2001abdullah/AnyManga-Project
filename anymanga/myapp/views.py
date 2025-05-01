31b42d76d01d69293211e42cc1c2aa53fd309c55
2001abdullah <abdullahbinhossain28@gmail.com>
2025-05-02 04:14:10 +0600

    update-will be complete soon

---
+++ anymanga/myapp/views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from .models import *
+from .forms import User_form
+
+# Create your views here.
+def home(request):
+    manga_preview=Manga.objects.all()
+    context={
+        'manga_preview':manga_preview
+    }
+    return render(request, template_name='myapp/home.html',context=context)
+
+def manga(request):
+    return render(request,template_name='myapp/manga.html')
+
+def merch(request):
+    return render(request,template_name='myapp/merch.html')
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Logged in successfully.")
+            return redirect('home')
+        else:
+            messages.error(request, "Invalid username or password.")
+
+    return render(request, 'myapp/login_view.html')
+
+
+def Register(request):
+      if request.method=='POST':
+          form=User_form(request.POST)
+          if form.is_valid():
+              form.save()
+              messages.success(request,"account opened successfully")
+              return redirect('home')
+          else:
+              messages.error(request,"invalid")
+      else:
+          form=User_form()
+      context={
+          'form':form
+      }
+      return render(request,template_name='myapp/register.html',context=context)
+
+            
